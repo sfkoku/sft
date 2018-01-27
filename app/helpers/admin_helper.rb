dfc80abb41a6d34811c361ecdbe1b80cc662bf82
chaptmn <loutihuji@icloud.com>
2018-01-28 01:59:59 +0900

    管理画面にダイジェスト認証,セッションヘルパにあるメソッドとの重複を修正

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,34 +1,20 @@
 module AdminHelper
-  def admin_login email, pass
-    admin_user = User.find_by(id: 1)
-    if admin_user.email == email && admin_user.decrypt(admin_user.password) == pass  
-        session[:admin] = true
-    else
-      User.all.each do |user| 
-        if email == user.email && pass == user.decrypt(user.password) 
-          p "okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
-          session[:user_id] = user.id
-        else
-          p "nooooooooooooooooooooooooooooooooooo"
-        end
-      end
-    end
-  end
 
   def admin_logout
     session[:admin] = nil
   end
 
-  def user_logout
-    session[:user_id] = nil
-  end
-
-  def is_admin?
+  def is_administrator?
     session[:admin].present?
   end
 
-  def is_login?
-    session[:user_id].present?
-  end
+  private
+
+    USERS = { ENV['USERNAME'] => ENV['SECRET'] }
+    def digest_auth
+        authenticate_or_request_with_http_digest do |user|
+          USERS[user]
+        end
+    end
 
 end
