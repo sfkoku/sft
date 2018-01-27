2704baf2f8aeedce1154bab6def880aac22f6fde
chaptmn <loutihuji@icloud.com>
2018-01-28 01:34:56 +0900

    ルーティングの変更とか

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,15 @@ Rails.application.routes.draw do
    #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    root 'root#index'
    resources :books, only: [:show]
-   resources :users
+   resources :users do
+     resources :logs, only: [:create, :index]
+   end
    resources :categories, only: [:index,:show]
-   resources :logs, only: [:create]
+   get    'login'   => 'sessions#new'
+   post   'login'   => 'sessions#create'
+   delete 'logout'  => 'sessions#destroy'
    namespace :admin do
      resources :books
-     resources :sessions, only:[:show,:create,:destroy]
      resources :logs, only: [:index,:show,:update,:destroy]
    end
 end
