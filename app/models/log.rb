a2698b125a2870666f2cd18aace5f145c72becc4
chaptmn <loutihuji@icloud.com>
2018-03-13 21:31:40 +0900

    logのdeleteを追加

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,8 @@ class Log < ApplicationRecord
   belongs_to :book
 
   enum is_processed: {x: 0,v: 1}
+
+  def self.get_current_user_log(user_id, book_id)
+    where(user_id: user_id).where(book_id: book_id)
+  end
 end
