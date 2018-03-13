355c7394d9024c9cdc6aab97b053d5bdfa17f968
chaptmn <loutihuji@icloud.com>
2018-03-13 20:00:34 +0900

    kaminariの導入

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,7 @@
 class RootController < ApplicationController
   def index
     @books = Book.where("count > 0").where(is_published: 1)
+    @books = @books.page(params[:page]).per(20)
     flash.now[:notice] 
   end
 end
