e71337fdf0833c2dd99b6f8e79891bf5a13df33c
chaptmn <loutihuji@icloud.com>
2018-03-18 19:19:28 +0900

    時間の追加

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,4 +7,8 @@ class Log < ApplicationRecord
   def self.get_current_user_log(user_id, book_id)
     where(user_id: user_id).where(book_id: book_id)
   end
+
+  def book_time
+    (self.created_at + 9.hours).strftime("%Y/%m/%d %H:%M")
+  end
 end
