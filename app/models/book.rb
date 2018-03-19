266fee56a05a0904ca96f1ec0869ddde6154bb85
chaptmn <loutihuji@icloud.com>
2018-03-20 00:54:23 +0900

    スコープ追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :logs, dependent: :destroy
   belongs_to :category
+
+  scope :get_by_title, ->(title){
+    where("title like ?", "%#{title}%")
+  }
+  scope :is_stocked, ->{ where("count > 0") }
+  scope :is_published, ->{ where(is_published: 1)}
 end
