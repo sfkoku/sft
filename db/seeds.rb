0208e2705f2260066e3c44fb5ac312eddb1f3785
chaptmn <loutihuji@icloud.com>
2018-04-01 20:40:07 +0900

    Feature/category pagination (#32)
    * kaminari関連の変更
    
    * seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Book.create(title: "テスト",
+              author: "テスト著者",
+              price: 10000,
+              category_id: 1,
+              count: 1,
+              image: open("#{Rails.root}/public/uploads/book/test.jpg"), 
+              is_published: true)
+end
