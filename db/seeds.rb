93aaa0ed013d680f22d0b898b4f81eeb74bf0207
chaptmn <loutihuji@icloud.com>
2018-04-01 20:36:32 +0900

    seed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Book.create(title: "新しいやつ",
+              author: "利光",
+              price: 10000,
+              category_id: 3,
+              count: 5,
+              image: open("#{Rails.root}/public/uploads/book/image/20/8CE08AC0-0029-4354-A3E8-F09035C901EE.jpeg"), 
+              is_published: true)
+end
