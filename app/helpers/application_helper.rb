a9af5d0fe9750fcf25f37d1de805d1e8d38c4955
chaptmn <loutihuji@icloud.com>
2018-04-07 19:47:53 +0900

    metaタグ設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: Settings.meta.name,
+      charset: 'utf-8',
+      title: Settings.meta.title,
+      description: Settings.meta.description,
+      keywords: Settings.meta.keywords,
+      canonical: request.original_url,
+      og: {
+        type: Settings.meta.og.type,
+        title: :title,
+        description: :description,
+        url: request.original_url,
+        site_name: :site,
+        image: "http://sftkoku.tokyo/#{asset_path("sft_logo3_1.png")}",
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'SFT國學院オンライン教科書予約',
+        desciption: :description,
+        creator: '@sftkoku',
+        image: "http://sftkoku.tokyo/#{asset_path("sft_logo3_1.png")}"
+      }
+    }
+  end
 end
